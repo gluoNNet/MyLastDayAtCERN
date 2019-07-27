ce66502ef4b3bb180eb578c2cd7477157a9a24a5
William <william_yam@outlook.com>
2019-07-27 21:42:57 +0200

    added

--- Database/retrieve_indico.py
+++ Database/retrieve_indico.py
@@ -1,6 +1,7 @@
 import requests
 import json5
 import time
+import os
 
 API_KEY = "879b1354-896f-4647-a021-86de07666f78"
 SECRET_KEY = "d79d0c29-5c7d-4e8a-9efe-0b183e773854"
@@ -8,33 +9,37 @@ SECRET_KEY = "d79d0c29-5c7d-4e8a-9efe-0b183e773854"
 
 if __name__ == '__main__':
 
-    t = time.localtime()
-    currentTime = time.strftime("%H:%M:%S", t)
-    print(currentTime)
-    #path = "https://indico.cern.ch/export/event/817571.json"
-    path = "https://indico.cern.ch/categ/345.json"
-    parameters = {
-        "pretty": "yes",
-        "onlypublic": "yes",
-        "limit": 10
-    }
-    response = requests.get("https://indico.cern.ch/export/categ/345.json?pretty=yes&onlypublic=yes")
+    for i in range(19):
+        print("Loop batch " + str(i))
+        t = time.localtime()
+        currentTime = time.strftime("%H:%M:%S", t)
+        print(currentTime)
 
-    data = {}
-    if response.status_code != 200:
-        print('Failed to get data:', response.status_code)
-    else:
-        data = response.json()
-        print(data["url"])
+        # path = "https://indico.cern.ch/export/event/817571.json"
+        # path = "https://indico.cern.ch/exprot/categ/345.json"
+        path = "https://indico.cern.ch/export/categ/6725.json"
+        parameters = {
+            "pretty": "yes",
+            "onlypublic": "yes",
+            "detail": "contributions",
+            "limit": 100,
+            "offset": 100*i
+        }
+        response = requests.get(path, parameters)
 
-    with open("data.json", "w") as outfile:
-        json5.dump(data, outfile, sort_keys=True, indent=4)
+        data = {}
+        if response.status_code != 200:
+            print('Failed to get data:', response.status_code)
+        else:
+            data = response.json()
+            print(data["url"])
 
-    with open("data.json", "r") as infile:
-        data2 = json5.load(infile)
+        os.mkdir("data_json")
+        with open("data_json/data_{num}.json".format(num=str(i)), "w") as outfile:
+            json5.dump(data, outfile, sort_keys=True, indent=4)
 
-    print(type(data2))
-    t = time.localtime()
-    currentTime = time.strftime("%H:%M:%S", t)
-    print(currentTime)
+        print("Saved " + str(type(data)))
+        t = time.localtime()
+        currentTime = time.strftime("%H:%M:%S", t)
+        print(currentTime)
 
